571e75d9e416c46dd41f760410a874ca0e032b61
igor <108681204+igormieski27@users.noreply.github.com>
2023-08-04 13:56:56 -0300

    Create welcome.py

---
+++ welcome.py
@@ -0,0 +1,4 @@
+def display_welcome_message():
+    print("Bem-vindo ao aplicativo de previsão do tempo!")
+    print("Este aplicativo foi criado por igormieski27.")
+    print("GitHub: https://github.com/igormieski27\n")
